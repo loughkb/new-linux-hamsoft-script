a9ef54be7f453af0690f5fca2e746bf82c9df85e
loughkb <loughkb@yahoo.com>
2024-07-08 10:40:07 -0700

    Add files via upload

--- hamsoft.sh
+++ hamsoft.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt upgrade
 # Now we install the ham packages from the repository.
-sudo apt install fldigi flrig flwrap js8call gnuradio gqrx-sdr multimon-ng freedv linpac ax25-apps cubicsdr qsstv wsjtx xlog gpredict chirp cutesdr grig wfview 
+sudo apt install fldigi flrig flwrap js8call gnuradio gqrx-sdr multimon-ng freedv linpac ax25-apps cubicsdr qsstv wsjtx xlog gpredict chirp cutesdr grig wfview trustedqsl 
 # Now we remove brltty if it's installed so it doesn't grab every serial device we plug in.
 sudo apt remove brltty
 # Finally, we add our username to the dialout group so we have access to serial devices.
